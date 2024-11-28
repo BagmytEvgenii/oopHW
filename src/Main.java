bd94f46525ad2f1f25f481fa4046d83385c4a1a0
Evgenyi Bagmut PC <jenya.bagmyt@yandex.ru>
2024-11-29 00:08:05 +0300

    hw1

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,20 @@
-import java.util.List;
-
+import model.FamilyTree;
+import model.Person;
+import presenter.CommandHandler;
+import presenter.TreePresenter;
+import service.FileOperationsImpl;
+import view.ConsoleTreeView;
 public class Main {
     public static void main(String[] args) {
-        FamilyTree familyTree = new FamilyTree();
-// Создаем людей
-        Person john = new Person("John", 1950);
-        Person mary = new Person("Mary", 1955);
-        Person susan = new Person("Susan", 1980);
-        Person drake = new Person("Drake", 1999);
-// Устанавливаем родительские связи
-        drake.setFather(john);
-        drake.setMother(mary);
-        susan.setMother(mary);
-        susan.setFather(john);
-        john.addChild(susan);
-        john.addChild(drake);
-        mary.addChild(susan);
-        mary.addChild(drake);
-// Добавляем людей в древо
-        familyTree.addPerson(john);
-        familyTree.addPerson(mary);
-        familyTree.addPerson(susan);
-        familyTree.addPerson(drake);
-// Пример получения всех детей Джона
-        List<Person> johnsChildren = familyTree.getChildren(john);
-        for (Person child : johnsChildren) {
-            System.out.println("John's child: " + child.getName());
-        }
+        FamilyTree<Person> familyTree = new FamilyTree<>();
+        ConsoleTreeView view = new ConsoleTreeView();
+        FileOperationsImpl<Person> fileOperations = new
+                FileOperationsImpl<>();
+        TreePresenter presenter = new TreePresenter(familyTree, view, view, view, fileOperations);
+        CommandHandler commandHandler = new CommandHandler(presenter, view);
+        commandHandler.handleUserInput();
     }
 }
+
+
+
---
+++ src/service/FileOperations.java
@@ -0,0 +1,7 @@
+package service;
+import model.FamilyTree;
+import java.io.IOException;
+public interface FileOperations<T> {
+    void saveToFile(FamilyTree<T> familyTree, String fileName) throws IOException;
+    FamilyTree<T> loadFromFile(String fileName) throws IOException, ClassNotFoundException;
+}
